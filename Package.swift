85f19c9f3737a406ed61a6c095118907d4297a28
Tim <0xtimc@gmail.com>
2023-11-09 11:14:26 +0100

    Bring in JWTKit 5

--- Package.swift
+++ Package.swift
@@ -1,24 +1,24 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 // Copyright (c) 2023 Apple Inc. Licensed under MIT License.
 
 import PackageDescription
 
-
 let package = Package(
     name: "AppStoreServerLibrary",
     platforms: [
-        .macOS(.v11), // And other server environments
+        .macOS(.v13), // And other server environments
     ],
     products: [
         .library(
             name: "AppStoreServerLibrary",
-            targets: ["AppStoreServerLibrary"]),
+            targets: ["AppStoreServerLibrary"]
+        ),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-certificates.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-asn1.git", from: "1.1.0"),
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "4.0.0"),
-        .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/jwt-kit.git", branch: "jwtkit-5"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-nio", from: "2.0.0"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.9.0"),
@@ -34,7 +34,8 @@ let package = Package(
                 .product(name: "AsyncHTTPClient", package: "async-http-client"),
                 .product(name: "NIOFoundationCompat", package: "swift-nio"),
                 .product(name: "NIOHTTP1", package: "swift-nio"),
-            ]),
+            ]
+        ),
         .testTarget(
             name: "AppStoreServerLibraryTests",
             dependencies: ["AppStoreServerLibrary"],
