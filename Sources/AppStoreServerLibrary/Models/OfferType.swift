1cea28d52de34c794c13fee12e0f540196a10ab7
Alex Baker <alexander_j_baker@apple.com>
2024-07-09 08:59:56 -0600

    Add support for App Store Server API v1.13 and App Store Server Notifications v2.13     https://developer.apple.com/documentation/appstoreserverapi/app_store_server_api_changelog     https://developer.apple.com/documentation/appstoreservernotifications/app_store_server_notifications_changelog

--- Sources/AppStoreServerLibrary/Models/OfferType.swift
+++ Sources/AppStoreServerLibrary/Models/OfferType.swift
@@ -7,4 +7,5 @@ public enum OfferType: Int32, Decodable, Encodable, Hashable {
     case introductoryOffer = 1
     case promotionalOffer = 2
     case subscriptionOfferCode = 3
+    case winBackOffer = 4
 }
